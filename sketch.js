5c05b12ef651fa9366f7fdbcb2dc7e9290be90e5
demscephei <demiesco23@gmail.com>
2021-08-19 22:43:49 -0300

    Added level select and reset function.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,91 @@
+let animX;
+let animY;
+let speed = 1;
+
+const levelList =[
+	{
+		"start": [
+			[0, 0, 1],
+			[0, 0, 0],
+			[1, 0, 1],
+		],
+		"goal": [
+			[1, 0, 0],
+			[9, 1, 0],
+			[0, 0, 1]
+		]
+	},
+{
+		"start": [
+			[2, 0, 0],
+			[2, 2, 1],
+			[1, 0, 0],
+			[0, 1, 1],
+			[0, 0, 1]
+		],
+		"goal": [
+			[1, 0, 1],
+			[1, 0, 0],
+			[0, 2, 1],
+			[0, 2, 0],
+			[1, 2, 0]
+		]
+	}
+]
+
 function setup() {
 	let canvas = createCanvas(480, 480);
 	canvas.parent('game-container');
-	// put setup code here
+	// animX = width / tileMap[0].length / 2
+	// animY = height / tileMap.length / 2
 }
-// Starting Map (where the tokens are positioned at the start)
-let startingMap = [
-	[2, 0, 0, 0, 0],
-	[2, 2, 1, 0, 0],
-	[1, 0, 0, 0, 0],
-	[0, 1, 1, 0, 0],
-	[0, 1, 1, 0, 0],
-	[0, 0, 1, 0, 0]
-]
-
-// Goal Map (where the tokens should be to win the game)
-let goalMap = [
-	[1, 0, 9, 0, 0],
-	[1, 0, 0, 0, 1],
-	[0, 2, 1, 0, 0],
-	[9, 2, 0, 9, 0],
-	[9, 2, 0, 9, 0],
-	[1, 2, 0, 0, 0]
-]
-
-
-// TileMap gets assigned Starting Map (for future game resetting)
-let tileMap = new Array()
-tileMap = startingMap
-
-// Command history for when you press "UP" on the input field
-let cmdHistory = []
-
 
 // Gets input field and status ("You win!") elements
 const input = document.getElementById("input-field")
 const status = document.getElementById("status");
+const btnList = document.getElementById("button-list");
+
+let tileMap = new Array();
+let startingMap;
+let goalMap;
+
+let levelButtons = [1,2]
+
+function createBtn(){
+	for(let b = 0; b < levelButtons.length; b++){
+		let btn = document.createElement("button")
+		let txt = document.createTextNode("Level " + levelButtons[b])
+		// btn.innerHTML = "Level "
+		btn.appendChild(txt)
+		btn.addEventListener("click",function(){
+			loadLevel(b)
+		})
+		btnList.appendChild(btn)
+		// console.log("Level " + levelButtons[b])
+	}
+}
+
+createBtn()
+
+function loadLevel(level) {
+		// Starting Map (where the tokens are positioned at the start)
+		startingMap = JSON.parse(JSON.stringify(levelList[level].start));
+
+		// Goal Map (where the tokens should be to win the game)
+		goalMap = JSON.parse(JSON.stringify(levelList[level].goal));
+
+		// TileMap gets assigned Starting Map (for future game resetting)
+		tileMap = JSON.parse(JSON.stringify(startingMap));
+}
+
+loadLevel(0)
+
+function resetLevel(){
+	tileMap = JSON.parse(JSON.stringify(startingMap));
+}
+
+// Command history for when you press "UP" on the input field
+let cmdHistory = []
 
 // Dictionary with all the commands you can call
 const funcDict = {
@@ -42,16 +93,17 @@ const funcDict = {
 	down: function (val) { moveDown(val) },
 	left: function (val) { moveLeft(val) },
 	right: function (val) { moveRight(val) },
-	undo: function (val) { console.log("undoed") }
+	reset: function () { resetLevel() },
+	undo: function () { console.log("undoed") }
 }
 
+// Makes the "command line" work when you press the Enter key
 input.addEventListener("keyup", (event) => {
 	if (event.keyCode === 13){
 		event.preventDefault();
 
 	  const [direction, val] = input.value.split(' ');
 	  const fn = funcDict[direction.toLowerCase()];
-
 
 	  if (fn) {
 	    fn(val);
@@ -67,6 +119,7 @@ input.addEventListener("keyup", (event) => {
 	}
 });
 
+// Checks if there's a wall on the right
 function checkMoveRight(row){
 	let canMove = true
 	tileMap[row].forEach((item,index) => {
@@ -80,6 +133,7 @@ function checkMoveRight(row){
 	return canMove
 }
 
+// Checks if there's a wall on the left
 function checkMoveLeft(row){
 	let canMove = true
 	tileMap[row].forEach((item,index) => {
@@ -93,6 +147,7 @@ function checkMoveLeft(row){
 	return canMove
 }
 
+// Checks if there's a wall above
 function checkMoveUp(col){
 	let canMove = true
 	tileMap.forEach((item,index) => {
@@ -109,6 +164,7 @@ function checkMoveUp(col){
 	return canMove
 }
 
+// Checks if there's a wall below
 function checkMoveDown(col){
 	let canMove = true
 	tileMap.forEach((item,index) => {
@@ -125,6 +181,7 @@ function checkMoveDown(col){
 	return canMove
 }
 
+// Gets the current column
 function getColumn(col){
     newColumn = new Array()
     for (let i = 0; i < tileMap.length; i++){
@@ -137,6 +194,7 @@ function getColumn(col){
     return newColumn
 }
 
+// Gets the current row (unused)
 function getRow(row){
     newRow = new Array()
     for (let i = 0; i < tileMap.length; i++){
@@ -147,15 +205,18 @@ function getRow(row){
     return newRow
 }
 
+// Moves the current row to the left once
 function moveLeft(row){
     if (tileMap[row][0] === 0 && checkMoveLeft(row)){
-        let item = tileMap[row].shift()
-        tileMap[row].push(item)
+      let item = tileMap[row].shift()
+      tileMap[row].push(item)
+			// animX -= 1 * tileWidth
     }
     // console.log(tileMap)
 		return checkArrays(tileMap,goalMap)
 }
 
+// Moves the current row to the right once
 function moveRight(row){
 	    if (tileMap[row][tileMap[row].length - 1] === 0 && checkMoveRight(row)){
 	        let item = tileMap[row].pop()
@@ -165,6 +226,7 @@ function moveRight(row){
 			return checkArrays(tileMap,goalMap)
 }
 
+// Moves the current column up once
 function moveUp(col){
 	if (col < tileMap.length){
 	    let column = getColumn(col)
@@ -183,6 +245,7 @@ function moveUp(col){
 	return checkArrays(tileMap,goalMap)
 }
 
+// Moves the current column down once
 function moveDown(col){
 	if (col < tileMap.length){
 	    let column = getColumn(col)
@@ -201,6 +264,7 @@ function moveDown(col){
 	return checkArrays(tileMap,goalMap)
 }
 
+// Checks both map arrays to see if the player won
 const checkArrays = function(arr1, arr2) {
   if (arr1.toString() === arr2.toString()){
 		status.textContent = "You win!"
@@ -211,7 +275,6 @@ const checkArrays = function(arr1, arr2) {
 /// DRAW STUFF ////////////////////////////////////////////////////
 function draw() {
 	background(23, 19, 42)
-	// put drawing code here
 	tileWidth = width / tileMap[0].length
 	tileHeight = height / tileMap.length
 	tilePadding = 8
@@ -226,9 +289,6 @@ function draw() {
 		aspectRatio = tileWidth / tileHeight
 		tileHeight = tileHeight * aspectRatio
 	}
-
-	// tileWidth = tileWidth / aspectRatio
-
 
 ///// Board ///////////////////////////////////////////////
 	for (let i = 0; i < goalMap.length; i++){
@@ -278,18 +338,27 @@ function draw() {
 					fill(253, 252, 220,255)
 					rect(x,y,tileWidth,tileHeight)
 				}
-
-				// ellipse(x+width,y+width,width)
 			}
 		}
 
 	///// Tokens ////////////////////////////////////////////
+
+	// animX += speed;
+	// ellipse(animX,128,32)
+
 	for (let i = 0; i < tileMap.length; i++){
 			for(let j = 0; j < tileMap[i].length; j++){
 				let x = j * tileWidth
-				let y = i *tileHeight
+				let y = i * tileHeight
 				let token = tileMap[i][j]
+
+				// animX[j] = x[j]
 //////////////////////////////////////////////////////////
+
+				// animX = x
+				// if (animX[j] < x[j]) {
+				// 	animX[j] -= speed
+				// }
 
 				// strokeWeight(4)
 				fill(255,0)
@@ -325,10 +394,6 @@ function draw() {
 					stroke(249, 251, 61,40)
 					ellipse(x+tileWidth/2,y+tileHeight/2,tileHeight-8)
 				}
-
-				// rect(x,y,width,width)
-
-
 		}
 	}
 }
